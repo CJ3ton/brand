926536d9634114331318eb887539bb5a8f0faae9
cj3ton <cj3ton@gmail.com>
2022-02-19 17:06:35 +0700

    Mobile version completed

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.modal__dialog-close'),
         button = document.querySelector('.heading__btn'),
-        body = document.querySelector('body');
+        body = document.querySelector('body'),
+        form = document.querySelector('.modal__dialog-form'),
+        burger = document.querySelector('.burger'),
+        mobile = document.querySelector('.mobile'),
+        menuItems = mobile.querySelectorAll('li'),
+        mobileClose = mobile.querySelector('.mobile__close');
 
     close.addEventListener('click', () => {
         overlay.classList.remove('show');
+        modal.classList.remove('open');
         body.classList.remove('fixed');
     });
 
@@ -15,20 +21,81 @@ document.addEventListener('DOMContentLoaded', () => {
         modal.classList.add('open');
     });
 
-    overlay.addEventListener('click', (e) => {
-        if(e.target !== modal) {
+    burger.addEventListener('click', () => {
+        overlay.classList.add('show');
+        mobile.classList.add('opened');
+    });
+
+    mobileClose.addEventListener('click', () => {
         overlay.classList.remove('show');
-        body.classList.remove('fixed');
+        mobile.classList.remove('opened');
+    });
+
+    menuItems.forEach((item) => {
+        item.addEventListener('click', () => {
+            overlay.classList.remove('show');
+            mobile.classList.remove('opened');
+        });
+    });
+
+    overlay.addEventListener('click', (e) => {
+        console.log(e.target);
+        if (e.target === overlay) {
+            overlay.classList.remove('show');
+            body.classList.remove('fixed');
+            modal.classList.remove('open');
+            mobile.classList.remove('opened');
         }
     });
 
-});
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.reviews__slider-next',
+            prevEl: '.reviews__slider-prev',
+        },
+    });
 
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    navigation: {
-        nextEl: '.reviews__slider-next',
-        prevEl: '.reviews__slider-prev',
-    },
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        let response = await fetch('/mailer/smart.php', {
+            method: 'POST',
+            body: new FormData(form)
+        });
+
+        let result = await response.json();
+
+        if (result.message === 'success') {
+            console.log('Success!');
+            form.reset();
+            overlay.classList.remove('show');
+            modal.classList.remove('open');
+            body.classList.remove('fixed');
+            showMessage('Uw bestelling is geaccepteerd! Wij bellen u terug!');
+        } else {
+            console.log('Error!');
+            form.reset();
+            overlay.classList.remove('show');
+            modal.classList.remove('open');
+            body.classList.remove('fixed');
+            showMessage('Er is iets fout gegaan!');
+        }
+
+    });
+
+    function showMessage(text) {
+        const message = document.querySelector('.message');
+        message.textContent = text;
+        overlay.classList.add('show');
+        message.classList.add('open');
+        const timerId = setTimeout(closeMessage, 5000);
+
+        function closeMessage() {
+            message.classList.remove('open');
+            overlay.classList.remove('show');
+            clearTimeout(timerId);
+        }
+    }
 
 });
